19bbea30cae1fd90dbf34e41880d10caceff504b
Jagger78 <floescher@aon.at>
2024-04-10 14:51:59 +0200

    Update scraper.pl

--- scraper.pl
+++ scraper.pl
@@ -1,36 +1,37 @@
-# This is a template for a Perl scraper on morph.io (https://morph.io)
-# including some code snippets below that you should find helpful
+use LWP::Simple;
+use HTML::TreeBuilder;
+use Database::DumpTruck;
 
-# use LWP::Simple;
-# use HTML::TreeBuilder;
-# use Database::DumpTruck;
+use strict;
+use warnings;
 
-# use strict;
-# use warnings;
+# Turn off output buffering
+$| = 1;
 
-# # Turn off output buffering
-# $| = 1;
+my $stringToFind = "XXXXXXXX";
 
-# # Read out and parse a web page
-# my $tb = HTML::TreeBuilder->new_from_content(get('http://example.com/'));
+# Read out and parse a web page
+my $tb = HTML::TreeBuilder->new_from_content(get('https://www.win2day.at/gewinner-des-tages/'));
 
-# # Look for <tr>s of <table id="hello">
-# my @rows = $tb->look_down(
-#     _tag => 'tr',
-#     sub { shift->parent->attr('id') eq 'hello' }
-# );
+# Look for <tr>s of <table id="hello">
+my @rows = $tb->look_down(
+    _tag => 'tr',
+    sub { shift->parent->attr('id') eq 'hello' }
+); 
 
-# # Open a database handle
-# my $dt = Database::DumpTruck->new({dbname => 'data.sqlite', table => 'data'});
-#
-# # Insert some records into the database
-# $dt->insert([{
-#     Name => 'Susan',
-#     Occupation => 'Software Developer'
-# }]);
+# Check if our string was found
+foreach my $row (@rows) {
+    if($row->as_text =~ m/\Q$stringToFind\E/) {
+        # print "String gefunden: $stringToFind\n";
+        last;
+    }
+}
 
-# You don't have to do things with the HTML::TreeBuilder and Database::DumpTruck libraries.
-# You can use whatever libraries you want: https://morph.io/documentation/perl
-# All that matters is that your final data is written to an SQLite database
-# called "data.sqlite" in the current working directory which has at least a table
-# called "data".
+# Open a database handle
+my $dt = Database::DumpTruck->new({dbname => 'data.sqlite', table => 'data'});
+
+# Insert some records into the database
+$dt->insert([{
+    Name => 'Susan',
+    Occupation => 'Software Developer'
+}]);
